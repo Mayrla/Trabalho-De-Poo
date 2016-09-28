94c9e621dc54327cc5cfeacf8edc8b9c86a3fac5
Mayrla <mayrlasillva57@gmail.com>
2016-09-28 09:21:56 -0300

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,1 +1,20 @@
 
+public class Paciente{
+	private static numDeEntradas;
+	private String nome;
+	private String sexo;
+	private int idade;
+	private int numDoPaciente;
+	private String doencasEAlergia;
+	// importar ArrayList
+	private ArrayList<String> historicoHospitalar;
+	
+	public Paciente(String nome,String sexo,int idade,String doencasEAlergia){
+		Paciente.numDeEntradas++;
+		numDoPaciente= numDeEntradas;
+		this.nome=nome;
+		this.sexo=sexo;
+		this.idade=idade;
+		this.doencasEAlergia=doencasEAlergia;
+	}
+}
