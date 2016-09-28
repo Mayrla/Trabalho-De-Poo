c4d54bc4b70edd38e6c5cd1e370ae5da4f04ad9e
Mayrla <mayrlasillva57@gmail.com>
2016-09-28 08:30:32 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Principal{
+	public static void main(String args[]){
+		Scanner sc =new scanner(System.in);
+		System.out.println("Digite quantos usuarios seram cadastrados:");
+		
+		for(int i=0;i<sc.nextInt();i++){
+			
+
+		}
+	}
+}
