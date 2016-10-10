af2fc170e2af30703d775cd058d718555be739a4
Mayrla <mayrlasillva57@gmail.com>
2016-10-10 09:24:58 -0300

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -1,1 +1,18 @@
 
+ public class Hospital{
+        private ArrayList<Paciente> paciente;
+	private Scanner sc = new Scanner(System.in);
+	 private Scanner scl = new Scanner(System.in);
+	 
+	 public void entrada(){
+		 System.out.println("Digite o nome:");
+		 String nome = scl.nextLine();
+		 System.out.println("Digite sua idade:");
+		 int idade = sc.nextInt();
+		 System.out.println("Digite o sexo:");
+		 String sexo = sc.next();
+		 System.out.println("Digite Doencas cronicas ou alergias:");
+	 }
+ }
+		 
+	 
