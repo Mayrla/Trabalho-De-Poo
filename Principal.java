90537a922e512f7ad553c7ce7c02e09c827faddd
Mayrla <mayrlasillva57@gmail.com>
2016-10-11 09:22:22 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Principal{
+	public static Hospital h;
 	public static void main(String args[]){
+		h=new Hospital();
 		menu();
 	}
 	public static void menu(){
@@ -10,6 +12,20 @@ public class Principal{
 			"[3]Situacao do paciente/n"+
 			"[4]Sair"
 		);
+		acao();
+	}
+	public static void acao(){
+		Scanner sc = new Scanner(System.in);
+		swicth( sc.nextInt()){
+			case 1:h.entrada();break;
+			case 2:h.saida();break;
+			case 3:h.situacaoDoPaciente();break;
+			case 4:
+			System.out.println("Obrigado por usar este programa");
+			return;
+			break;
+			default:System.out.println("Opcao invalida");
+		}
 		
 	}
 }
